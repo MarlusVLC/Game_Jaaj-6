00d1d5c81f5b10b5a00d8b1d7e9adc471cab5a5e
Choosecake <imchoosecake@gmail.com>
2021-07-24 22:17:22 -0300

    faz a base para o controle dos objetos

---
+++ Assets/Scripts/ObjectController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour
+{
+    [SerializeField] private Transform currentObject;
+    private float rotateSpeed = 100f;
+    void Update()
+    {
+        // rotating the current object using the horizontal and vertical inputs
+        currentObject.transform.Rotate(Input.GetAxisRaw("Vertical") * rotateSpeed * Time.deltaTime, 
+            -Input.GetAxisRaw("Horizontal") * rotateSpeed * Time.deltaTime, 0f, Space.World);
+        
+    }
+}
