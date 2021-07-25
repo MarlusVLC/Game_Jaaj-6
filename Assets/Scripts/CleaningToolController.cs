4e88ed8b1a4619c562b3949759032875ff07d3e1
Choosecake <imchoosecake@gmail.com>
2021-07-25 16:18:24 -0300

    faz a base para o highlight da prateleira (ainda precicsa colocar o script no objeto de prateleira)

---
+++ Assets/Scripts/CleaningToolController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// https://www.youtube.com/watch?v=0yHBDZHLRbQ&t=97s
+
+public class CleaningToolController : MonoBehaviour
+{
+    private Vector3 mOffset;
+    private float mZCoord;
+    
+    void OnMouseDown()
+    {
+        mZCoord = Camera.main.WorldToScreenPoint(
+            gameObject.transform.position).z;
+
+        // Store offset = gameobject world pos - mouse world pos
+        mOffset = gameObject.transform.position - GetMouseAsWorldPoint();
+    }
+
+    private Vector3 GetMouseAsWorldPoint()
+    {
+        // Pixel coordinates of mouse (x,y)
+        Vector3 mousePoint = Input.mousePosition;
+
+        // z coordinate of game object on screen
+        mousePoint.z = mZCoord;
+
+        // Convert it to world points
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    void OnMouseDrag()
+    {
+        transform.position = GetMouseAsWorldPoint() + mOffset;
+    }
+}
