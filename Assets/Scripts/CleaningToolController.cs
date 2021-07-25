c05daa20731fe75012313db19346b5a1d22bc434
Choosecake <imchoosecake@gmail.com>
2021-07-25 01:22:41 -0300

    muda o script (CleaningToolController) e conserta o problema de colisão

--- Assets/Scripts/CleaningToolController.cs
+++ Assets/Scripts/CleaningToolController.cs
@@ -1,32 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- 
-public class CleaningToolController : MonoBehaviour {
- 
-    Rigidbody rb;
- 
-    void Start () {
- 
-        rb = GetComponent<Rigidbody>();
-    }
- 
-    private void OnMouseDrag()
+
+public class CleaningToolController : MonoBehaviour
+{
+    private Vector3 mOffset;
+    private float mZCoord;
+    
+    void OnMouseDown()
     {
-        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z + transform.position.z);
- 
-        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
- 
-        transform.position = objPosition;
- 
-        rb.isKinematic = true;
- 
+        mZCoord = Camera.main.WorldToScreenPoint(
+            gameObject.transform.position).z;
+
+        // Store offset = gameobject world pos - mouse world pos
+        mOffset = gameObject.transform.position - GetMouseAsWorldPoint();
     }
- 
-    private void OnMouseUp()
+
+    private Vector3 GetMouseAsWorldPoint()
     {
-        rb.isKinematic = false;
+        // Pixel coordinates of mouse (x,y)
+        Vector3 mousePoint = Input.mousePosition;
+
+        // z coordinate of game object on screen
+        mousePoint.z = mZCoord;
+
+        // Convert it to world points
+        return Camera.main.ScreenToWorldPoint(mousePoint);
     }
- 
- 
+
+    void OnMouseDrag()
+    {
+        transform.position = GetMouseAsWorldPoint() + mOffset;
+    }
 }
