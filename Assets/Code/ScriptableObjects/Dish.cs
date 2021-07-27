28ff8b9893fb5f2612453113904f2321b289c965
Marlus Vinicius <marlusvinicius2014@outlook.com>
2021-07-26 22:46:35 -0300

    Implementa sistema de apagar mancha em contato com louca

---
+++ Assets/Code/ScriptableObjects/Dish.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Code.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "New Utensil", menuName = "Utensil", order = 0)]
+    public class Dish : ScriptableObject
+    {
+        [SerializeField] private Utensil[] cleanerUtensils;
+    }
+}
---
+++ Assets/Scripts/ItemInteraction/TEST_StainErase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace ItemInteraction
+{
+    public class TEST_StainErase : MonoBehaviour
+    {
+
+        [Range(0,1)][SerializeField] float erasingFactor = 0.1f;
+        
+        private Material _decalMAT;
+        private Vector4 _alphaRemap;
+
+        private void Awake()
+        {
+            _decalMAT = GetComponent<MeshRenderer>().material;
+            _alphaRemap = _decalMAT.GetVector("_AlphaRemap");
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.transform.CompareTag("CleaningTool"))
+            {
+                ReduceAlpha();
+                if (_alphaRemap.y <= -0.9f)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+
+        private void ReduceAlpha()
+        {
+            _alphaRemap.y -= erasingFactor;
+            _decalMAT.SetVector("_AlphaRemap", _alphaRemap);
+        }
+    }
+}
