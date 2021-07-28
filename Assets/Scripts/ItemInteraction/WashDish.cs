ff47896abb18ebc67a67b51be141d995fc707319
Choosecake <imchoosecake@gmail.com>
2021-07-27 21:11:47 -0300

    faz melhorias no script de mergulhar a louça
    agora está smooth

--- Assets/Scripts/ItemInteraction/WashDish.cs
+++ Assets/Scripts/ItemInteraction/WashDish.cs
@@ -1,24 +1,17 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 
 public class WashDish : MonoBehaviour
 {
-	/*
-    public float force = 600;
-    public float damping = 6;
-
-    private Transform jointTrans;
-    private float dragDepth;
-    */
-	
-    public Transform sink;
+	public Transform sink;
     private Vector3 sinkPosition;
     private Vector3 startingPosition;
-    private float washForce = 5f;
+    private float washForce = 8f;
 
     private void Start()
     {
@@ -30,66 +23,11 @@ public class WashDish : MonoBehaviour
     {
 	    if (Input.GetKey("space"))
 	    {
-		    transform.position = Vector3.Lerp(startingPosition, sinkPosition, 1f);
+		    transform.position = Vector3.MoveTowards(transform.position, sinkPosition, washForce * Time.deltaTime);
 	    }
 	    else
 	    {
-		    transform.position = Vector3.Lerp(sinkPosition, startingPosition, 1f);
+		    transform.position = Vector3.MoveTowards(transform.position, startingPosition, washForce * Time.deltaTime);
 	    }
     }
-
-    /*
-    public void HandleInputBegin(Vector3 screenPosition)
-    {
-        var ray = Camera.main.ScreenPointToRay(screenPosition);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
-        {
-            if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Interactive"))
-            {
-                dragDepth = CameraPlane.CameraToPointDepth(Camera.main, hit.point);
-                jointTrans = AttachJoint(hit.rigidbody, hit.point);
-            }
-        }
-    }
-
-    public void HandleInput(Vector3 screenPosition)
-    {
-        if (jointTrans == null)
-            return;
-        var worldPos = Camera.main.ScreenToWorldPoint(screenPosition);
-        jointTrans.position = CameraPlane.ScreenToWorldPlanePoint(Camera.main, dragDepth, screenPosition);
-    }
-
-    Transform AttachJoint (Rigidbody rb, Vector3 attachmentPosition)
-	{
-		GameObject go = new GameObject ("Attachment Point");
-		go.hideFlags = HideFlags.HideInHierarchy; 
-		go.transform.position = attachmentPosition;
-		
-		var newRb = go.AddComponent<Rigidbody> ();
-		newRb.isKinematic = true;
-		
-		var joint = go.AddComponent<ConfigurableJoint> ();
-		joint.connectedBody = rb;
-		joint.configuredInWorldSpace = true;
-		joint.xDrive = NewJointDrive (force, damping);
-		joint.yDrive = NewJointDrive (force, damping);
-		joint.zDrive = NewJointDrive (force, damping);
-		joint.slerpDrive = NewJointDrive (force, damping);
-		joint.rotationDriveMode = RotationDriveMode.Slerp;
-		
-		return go.transform;
-	}
-
-    private JointDrive NewJointDrive(float force, float damping)
-    {
-        JointDrive drive = new JointDrive();
-        drive.mode = JointDriveMode.Position;
-        drive.positionSpring = force;
-        drive.positionDamper = damping;
-        drive.maximumForce = Mathf.Infinity;
-        return drive;
-    }
-    */
 }
