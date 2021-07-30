5aa1e7ae366532fc78f8e2c7b7fa083d2ef981dd
Choosecake <imchoosecake@gmail.com>
2021-07-30 00:55:23 -0300

    faz CleaningToolSelector funcionar
    YES!

---
+++ Assets/Scripts/ItemInteraction/CleaningToolSelector.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CleaningToolSelector : MonoBehaviour
+{
+    [SerializeField] private Transform sponge;
+    [SerializeField] private Transform steelSponge;
+    [SerializeField] private Transform cloth;
+    public Transform mainCleaningTool;
+
+    private Transform currentCleaningTool;
+
+    enum CleanerType
+    {
+        None,
+        Sponge,
+        SteelSponge,
+        Cloth
+    }
+
+    private CleanerType _selectedCleanerType;
+
+    public LayerMask Interactive;
+    Ray ray;
+    RaycastHit hit;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit, 1000f, ~Interactive))
+            {
+                switch (hit.collider.name)
+                {
+                    case "Sponge": _selectedCleanerType = CleanerType.Sponge;
+                        break;
+                    case "Steel Sponge": _selectedCleanerType = CleanerType.SteelSponge;
+                        break;
+                    case "Cloth": _selectedCleanerType = CleanerType.Cloth;
+                        break;
+                }
+            }
+            
+            SelectCleaningTool();
+        }
+    }
+
+    void SelectCleaningTool()
+    {
+        if (currentCleaningTool)
+        {
+            currentCleaningTool.gameObject.layer = 0;
+            currentCleaningTool.SetParent(null);
+        }
+        
+        switch (_selectedCleanerType)
+        {
+            case CleanerType.Sponge: 
+                currentCleaningTool = sponge;
+                break;
+            case CleanerType.SteelSponge: 
+                currentCleaningTool = steelSponge;
+                break;
+            case CleanerType.Cloth: 
+                currentCleaningTool = cloth;
+                break;
+        }
+
+        if (_selectedCleanerType != CleanerType.None)
+        {
+            currentCleaningTool.SetParent(mainCleaningTool);
+            currentCleaningTool.localPosition = Vector3.zero;
+            currentCleaningTool.gameObject.layer = LayerMask.NameToLayer("Interactive");
+        }
+    }
+}
