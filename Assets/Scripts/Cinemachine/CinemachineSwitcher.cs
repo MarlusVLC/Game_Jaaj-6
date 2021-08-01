3ea0563babe2559f3971350067a968989ed39acc
Choosecake <imchoosecake@gmail.com>
2021-08-01 03:05:13 -0300

    menu e cameras finalizados (faltando impedir que segure utensilios fora da camera de gameplay)

--- Assets/Scripts/Cinemachine/CinemachineSwitcher.cs
+++ Assets/Scripts/Cinemachine/CinemachineSwitcher.cs
@@ -8,35 +8,151 @@ public class CinemachineSwitcher : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera menuCamera;
     [SerializeField] private CinemachineVirtualCamera gameplayCamera;
+    [SerializeField] private CinemachineVirtualCamera creditsCamera;
+    [SerializeField] private CinemachineVirtualCamera itemsCamera;
+    [SerializeField] private CinemachineBrain mainCamera;
     [SerializeField] private Button playButton;
+    [SerializeField] private Button creditsButton;
+    [SerializeField] private Button itemsButton;
     [SerializeField] private PlayableAsset doorPivotTimeline;
     [SerializeField] private GameObject doorPivot;
     [SerializeField] private PlayableDirector director;
+    private bool isBlending;
+
+    private enum CurrentCamera
+    {
+        Menu,
+        Gameplay,
+        Credits,
+        Items
+    }
+    
+    CurrentCamera _currentCamera;
 
     private void Awake()
     {
-        playButton.onClick.AddListener(TaskOnClick);
+        playButton.onClick.AddListener(ClickPlayButton);
+        creditsButton.onClick.AddListener(ClickCreditsButton);
+        itemsButton.onClick.AddListener(ClickItemsButton);
 
         director = doorPivot.GetComponent<PlayableDirector>();
+
     }
 
-    private void TaskOnClick()
+    private void Update()
     {
-        SwitchPriority();
-        director.Play(doorPivotTimeline);
+        isBlending = mainCamera.IsBlending;
+        
+        if (Input.GetAxisRaw("Cancel") != 0 && !isBlending)
+        {
+            switch (_currentCamera)
+            {
+                case CurrentCamera.Gameplay:
+                    GameplayToMenu();
+                    break;
+                case CurrentCamera.Credits:
+                    CreditsToMenu();
+                    break;
+                case CurrentCamera.Items:
+                    ItemsToMenu();
+                    break;
+                case CurrentCamera.Menu:
+                    Application.Quit();
+                    break;
+            }
+        }
     }
 
-    private void SwitchPriority()
+    private void ClickPlayButton()
     {
-        if (menuCamera)
+        if (!isBlending)
+        {
+            MenuToGameplay();
+            director.Play(doorPivotTimeline);
+            _currentCamera = CurrentCamera.Gameplay;
+        }
+    }
+
+    private void ClickCreditsButton()
+    {
+        if (!isBlending)
+        {
+            MenuToCredits();
+            director.Play(doorPivotTimeline);
+            _currentCamera = CurrentCamera.Credits;
+        }
+    }
+    
+    private void ClickItemsButton()
+    {
+        if (!isBlending)
+        {
+            MenuToItems();
+            director.Play(doorPivotTimeline);
+            _currentCamera = CurrentCamera.Items;
+        }
+    }
+
+    private void MenuToGameplay()
+    {
+        if (_currentCamera == CurrentCamera.Menu)
         {
             menuCamera.Priority = 0;
             gameplayCamera.Priority = 1;
+            creditsCamera.Priority = 0;
+            itemsCamera.Priority = 0;
         }
-        else
+    }
+
+    private void MenuToCredits()
+    {
+        if (_currentCamera == CurrentCamera.Menu)
         {
-            menuCamera.Priority = 1;
+            menuCamera.Priority = 0;
             gameplayCamera.Priority = 0;
+            creditsCamera.Priority = 1;
+            itemsCamera.Priority = 0;
         }
+    }
+
+    private void MenuToItems()
+    {
+        if (_currentCamera == CurrentCamera.Menu)
+        {
+            menuCamera.Priority = 0;
+            gameplayCamera.Priority = 0;
+            creditsCamera.Priority = 0;
+            itemsCamera.Priority = 1;
+        }
+    }
+
+    private void GameplayToMenu()
+    {
+        _currentCamera = CurrentCamera.Menu;
+        director.Play(doorPivotTimeline);
+        menuCamera.Priority = 1;
+        gameplayCamera.Priority = 0;
+        creditsCamera.Priority = 0;
+        itemsCamera.Priority = 0;
+    }
+
+    private void CreditsToMenu()
+    {
+        _currentCamera = CurrentCamera.Menu;
+        director.Play(doorPivotTimeline);
+        menuCamera.Priority = 1;
+        gameplayCamera.Priority = 0;
+        creditsCamera.Priority = 0;
+        itemsCamera.Priority = 0;
+    }
+
+    private void ItemsToMenu()
+    {
+        _currentCamera = CurrentCamera.Menu;
+        director.Play(doorPivotTimeline);
+        menuCamera.Priority = 1;
+        gameplayCamera.Priority = 0;
+        creditsCamera.Priority = 0;
+        itemsCamera.Priority = 0;
     }
 }
